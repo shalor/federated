162ea56f35e7b7b4cd653ed86e40dab25754ffdf
shalor <orshalom32@gmail.com>
2023-09-07 17:36:49 +0300

    New results

--- PythonScript/remaining_commands.tcl
+++ PythonScript/remaining_commands.tcl
@@ -1,1 +1,1 @@
-python3.8 main.py --epochs 50 --agents 5 --batch-size 128 --results-file Attack5AgentsStr3ForComparison --attack --attack-type ConstantOutput --num-exp 100
+python3.8 main.py --epochs 50 --agents 5 --batch-size 128 --results-file Attack5AgentsStr3ForComparison --attack --attack-type ConstantOutput --num-exp 25 --seed 1e9
