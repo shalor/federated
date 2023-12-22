f4da467ee4fe6e60ec8aba7705435b0b77fb5bf9
shalor <orshalom32@gmail.com>
2023-12-22 18:14:02 +0200

    To PC2 from PC1

--- PythonScript/remaining_commands_pc1.tcl
+++ PythonScript/remaining_commands_pc1.tcl
@@ -1,5 +1,7 @@
 #python3.8 main.py --epochs 1000 --agents 5 --batch-size 128 --results-file MultipleUnsyncedAttackers --num-exp 1 --output-dir PC1_MultipleAttackersNotSynced
 #python3.8 main.py --epochs 300 --agents 10 --batch-size 128 --results-file MultipleUnsyncedAttackers --attack --attack-type ConstantOutput --num-exp 1 --output-dir PC1_MultipleAttackersNotSynced --attack-strength 1
 #python3.8 main.py --epochs 300 --agents 10 --batch-size 128 --results-file MultipleUnsyncedAttackers --attack --attack-type ConstantOutput --num-exp 1 --output-dir PC1_MultipleAttackersNotSynced
-python3.8 main.py --epochs 50 --agents 5 --batch-size 128 --results-file ROC --attack --attack-type ConstantOutput --num-exp 100 --output-dir PC1_ROC1_Plots --attack-strength 1
+python3.8 main.py --epochs 50 --agents  3 --batch-size 128 --results-file ROC --attack --attack-type ConstantOutput --num-exp 50 --output-dir NEW_PC1_ROC1_Plots_3Agents  --attack-strength 1 --seed 123213123
+python3.8 main.py --epochs 50 --agents  5 --batch-size 128 --results-file ROC --attack --attack-type ConstantOutput --num-exp 50 --output-dir NEW_PC1_ROC1_Plots_5Agents  --attack-strength 1 --seed 123213123
+python3.8 main.py --epochs 50 --agents 10 --batch-size 128 --results-file ROC --attack --attack-type ConstantOutput --num-exp 50 --output-dir NEW_PC1_ROC1_Plots_10Agents --attack-strength 1 --seed 123213123
 
