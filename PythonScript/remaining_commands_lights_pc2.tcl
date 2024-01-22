82997ddd07ee15b542391f670567e75fbefe45c1
Or Shalom <orshalom32@gmail.com>
2024-01-22 21:18:45 +0200

    pc2

--- PythonScript/remaining_commands_lights_pc2.tcl
+++ PythonScript/remaining_commands_lights_pc2.tcl
@@ -1,3 +1,3 @@
-python3.10 main_traffic_lights_pc2.py --epochs 50 --agents 5 --batch-size 128 --results-file lights --attack --attack-type ConstantOutput --num-exp 100 --output-dir PC2_LIGHTS_CONSTANT_STOP  --attack-strength 1 --seed 123213123
-python3.10 main_traffic_lights_pc2.py --epochs 50 --agents 5 --batch-size 128 --results-file lights --attack --attack-type ConstantOutput --num-exp 100 --output-dir PC2_LIGHTS_CONSTANT_STOP_DETECT  --attack-strength 1 --seed 123213123 --allow-detection
+#python3.10 main_traffic_lights_pc2.py --epochs 50 --agents 5 --batch-size 128 --results-file lights --attack --attack-type ConstantOutput --num-exp 100 --output-dir PC2_LIGHTS_CONSTANT_STOP  --attack-strength 1 --seed 123213123 --exp-start 90
+python3.10 main_traffic_lights_pc2.py --epochs 50 --agents 5 --batch-size 128 --results-file lights --attack --attack-type ConstantOutput --num-exp 100 --output-dir PC2_LIGHTS_CONSTANT_STOP_DETECT  --attack-strength 1 --seed 123213123 --allow-detection --exp-start 6
 
